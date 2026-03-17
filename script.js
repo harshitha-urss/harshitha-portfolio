2830d594c083952d01a7c108b0cdc59f57e41a22
Harshitha Urs <harshithaveerabhadraiah2642004@gmail.com>
2026-03-17 18:34:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -55,3 +55,23 @@ behavior: "smooth"
 html {
 scroll-behavior: smooth;
 }
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+let current = "";
+
+sections.forEach(section => {
+const sectionTop = section.offsetTop - 100;
+if (scrollY >= sectionTop) {
+current = section.getAttribute("id");
+}
+});
+
+navLinks.forEach(a => {
+a.classList.remove("active");
+if (a.getAttribute("href") === "#" + current) {
+a.classList.add("active");
+}
+});
+});
